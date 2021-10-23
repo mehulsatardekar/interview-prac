7d13bd7969dbf8840e73381e803ab4ddb8ff5462
Mehul satardekar <satardekarmehul042197@gmail.com>
2021-10-24 02:01:10 +0530

    question and some test question added

---
+++ arrowfunction/arrayFunction.js
@@ -0,0 +1,35 @@
+// const name =[];
+//  // add element to last
+// name.push(12);
+
+// name.push(122);
+
+// name.push("mehul")
+
+// // delete element from last
+// // name.pop()
+// // name.pop()
+// // name.pop()
+
+
+// // add element from front to an array 
+// name.unshift(11111);
+// name.unshift("alex");
+
+// // remove element from front to an array
+
+// name.shift();
+// name.shift();
+
+
+// console.log(name.length);
+// console.log(name);
+
+
+const arr =[];
+
+console.log(arr.push( [1, "mehul"] ) );
+console.log(arr.push( [2,"alex"] ) );
+
+console.log(arr[1]);
+console.log(arr);
---
+++ arrowfunction/arrowFunc.js
@@ -0,0 +1,29 @@
+
+function ks(name,num){
+    //console.log(name)
+     let sum = 5+5;
+     return "welcome "+name + sum;
+}
+
+const getname = function(name,num){
+    //console.log(name)
+     let sum = 5+5;
+     return "welcome "+name + sum;
+}
+
+
+const getname = (name,num)=>{
+    //console.log(name)
+     let sum = 5+5;
+     return "welcome "+name + sum;
+}
+const name = getname("hemant",12)
+
+console.log(name);
+
+
+// btn.addEventListener('click',(name, sk)=>{
+
+//     this.name = name;
+
+// })
